923eb7d55a114702586caa9a3b98f00945b3fbba
Subin Abid <io@sabid.in>
2025-11-18 19:53:22 +0530

    Add more calendars

--- test_app.py
+++ test_app.py
@@ -8,29 +8,27 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-# Test Env variables
-def test_env_variables():
-    """Test if environment variables are loaded correctly."""
-    assert os.getenv("GOOGLE_SHEET_ID") is not None
+class TestApp:
+    """Test suite for the app module."""
 
+    # Test Env variables
+    def test_env_variables(self):
+        """Test if environment variables are loaded correctly."""
+        assert os.getenv("GOOGLE_SHEET_ID") is not None
 
-# Test valid calendars
-def test_valid_calendars():
-    """Test if valid calendars are set correctly."""
-    assert isinstance(VALID_CALENDARS, list)
-    assert len(VALID_CALENDARS) == 6
-    for calendar in VALID_CALENDARS:
-        assert calendar.startswith("epgp17")
-        assert len(calendar) == 7
+    # Test valid calendars
+    def test_valid_calendars(self):
+        """Test if valid calendars are set correctly."""
+        assert isinstance(VALID_CALENDARS, list)
+        assert len(VALID_CALENDARS) == 12
+        for calendar in VALID_CALENDARS:
+            assert calendar.startswith("epgp17")
 
-
-# Test access to Google Sheet
-def test_google_sheet_access():
-    """Test if the Google Sheet is accessible."""
-    google_sheet_id = os.getenv("GOOGLE_SHEET_ID")
-    assert google_sheet_id is not None
-    url = (
-        f"https://docs.google.com/spreadsheets/d/{google_sheet_id}/gviz/tq?tqx=out:csv"
-    )
-    response = requests.get(url)
-    assert response.status_code == 200
+    # Test access to Google Sheet
+    def test_google_sheet_access(self):
+        """Test if the Google Sheet is accessible."""
+        google_sheet_id = os.getenv("GOOGLE_SHEET_ID")
+        assert google_sheet_id is not None
+        url = f"https://docs.google.com/spreadsheets/d/{google_sheet_id}/gviz/tq?tqx=out:csv"
+        response = requests.get(url)
+        assert response.status_code == 200
